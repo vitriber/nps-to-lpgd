31ae7c76a64fca5b18b7b9949fa53f5a928cbcaf
Vitor <vit.riber1@hotmail.com>
2021-08-13 14:37:47 -0300

    feature/criando algoritmo basico

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas
+from sklearn import linear_model
+
+df = pandas.read_csv("enterprises.csv")
+
+X = df[
+    [
+        'Question1', 
+        'Question2',
+        'Question3',
+        'Question4',
+        'Question5',
+        'Question6',
+        'Question7',
+        'Question8',
+        'Question9',
+        'Question10',
+        'Question11',
+        'Question12',
+        'Question13',
+        'Question14',
+        'Question15',
+    ]]
+y = df['NPS']
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+
+predictedNPS = regr.predict([[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]])
+
+print(predictedNPS)
